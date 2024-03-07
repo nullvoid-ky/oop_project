a62d6ba152905495c59d37f0d26bf27145fcb3dd
kanyok <134153117+nullvoid-ky@users.noreply.github.com>
2024-03-07 21:32:48 +0700

    come back home my hownlander

---
+++ script/script.js
@@ -0,0 +1,118 @@
+function createCard(username, picUrl, accountId) {
+    const cardList = document.getElementById("card-list")
+
+    // Create card element
+    const card = document.createElement("div")
+    card.classList.add("card")
+    card.dataset.accountId = accountId // Set accountId as a custom data attribute
+
+    // Create image element
+    const img = document.createElement("img")
+    img.src = picUrl
+    img.alt = "Profile Image"
+
+    // Create card detail element
+    const cardDetail = document.createElement("div")
+    cardDetail.classList.add("card-detail")
+
+    // Create card detail name element
+    const cardDetailName = document.createElement("div")
+    cardDetailName.classList.add("card-detail-name")
+    cardDetailName.textContent = username
+
+    const cardDetailLocation = document.createElement("div")
+    cardDetailLocation.classList.add("card-detail-location")
+    cardDetailLocation.textContent = "Bangkok"
+
+    // Append elements to card
+    cardDetail.appendChild(cardDetailLocation)
+    cardDetail.appendChild(cardDetailName)
+    card.appendChild(img)
+    card.appendChild(cardDetail)
+
+    // Add event listener to card
+    card.addEventListener("click", () => {
+        const accountId = card.dataset.accountId
+        console.log("Clicked card with accountId:", accountId)
+        // Perform GET request with accountId
+    })
+
+    // Append card to card list
+    cardList.appendChild(card)
+}
+
+function getMates() {
+    // Clear previous values
+    const cardList = document.getElementById("card-list")
+    cardList.innerHTML = ""
+
+    // Define request options
+    const requestOptions = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "x-token": my_token,
+        },
+        // body: JSON.stringify(requestBody)
+    }
+
+    fetch(url + "/api/controller/get-mates", requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+            data.data.forEach((item) => {
+                const username = item.account_detail.username
+                const picUrl = item.account_detail.pic_url
+                const accountId = item.account_detail.id
+                createCard(username, picUrl, accountId)
+            })
+        })
+        .catch((error) => console.error("Error fetching data:", error))
+}
+
+window.onload = function () {
+    getChatRooms()
+}
+function registerPage(buttonId) {
+    var encodedButtonId = encodeURIComponent(buttonId);
+    window.location.href = 'login.html?buttonId=' + encodedButtonId;
+}
+
+let my_token = '';
+const url = 'http://127.0.0.1:8000';
+// Add event listener to send button
+
+
+export function getCookie(cookieName) {
+    const name = cookieName + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    console.log(decodedCookie);
+    const cookieArray = decodedCookie.split(';');
+    for(let i = 0; i <cookieArray.length; i++) {
+        let cookie = cookieArray[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(name) === 0) {
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+    return "";
+}
+
+// Usage example
+const registrationData = getCookie('userData');
+console.log(registrationData); // Log the value retrieved from the cookie
+if (registrationData !== '') {
+    const data = JSON.parse(registrationData);
+    console.log(data);
+    my_token = data.token;
+    // Do something with the registration data
+} else {
+    console.log('Registration data not found in cookie.');
+}
+
+window.onload = function () {
+    getMates();
+}
+
+export default url;
